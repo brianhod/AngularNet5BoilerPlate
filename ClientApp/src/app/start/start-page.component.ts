c983dc68cc83c48655d46a2e6a2e862e7fc3a577
brian.hodgson <brian.hodgson@trapezegroup.com>
2021-03-23 10:20:33 +0100

    Intial Commit

---
+++ ClientApp/src/app/start/start-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+selector: 'start-page-component',
+templateUrl: './start-page.component.html',
+styleUrls: ['./start-page.component.css']
+})
+export class StartpageComponent implements OnInit {
+
+constructor() {
+}
+
+ngOnInit(){
+
+}
+}
